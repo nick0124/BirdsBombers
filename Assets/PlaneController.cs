da766c51407d7e95c8434861d9bacdc1678bc8ef
Никита <nick0124creator@gmail.com>
2023-11-23 22:45:16 +0500

    add basic movement

--- Assets/PlaneController.cs
+++ Assets/PlaneController.cs
@@ -8,12 +8,8 @@ public class PlaneController : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private float _rotationSpeed;
     [SerializeField] private float _rollAngle;
-    [SerializeField] private Transform _plane;
-    public float testVal;
-    float t;
-
-    private float _max;
-    private float _min;
+    [SerializeField] private Transform[] _planes;
+    public float testVal = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -28,22 +24,27 @@ public class PlaneController : MonoBehaviour
         Rigidbody rb = GetComponent<Rigidbody>();
         rb.velocity = transform.rotation * Vector3.left * _speed;
 
-        // t += 0.5f * Time.deltaTime;
-        // testVal = Mathf.Lerp(0, 1, t);
-
         if (Input.GetKey(KeyCode.A))
         {
             transform.eulerAngles -= Vector3.up * Time.deltaTime * _rotationSpeed;
-            
-            
+            if(testVal >= 0) testVal -= 1 * Time.deltaTime;
 
         }
         else if (Input.GetKey(KeyCode.D))
         {
             transform.eulerAngles += Vector3.up * Time.deltaTime * _rotationSpeed;
-            //_plane.localEulerAngles = Vector3.MoveTowards(_plane.localEulerAngles, new Vector3(10,0,0),10 * Time.deltaTime)*-1;
+            if(testVal <= 1) testVal += 1 * Time.deltaTime;
+            
+        }else{
+            if(testVal < 0.5) testVal += 1 * Time.deltaTime;
+            if(testVal > 0.5) testVal -= 1 * Time.deltaTime;
+
         }
 
-        _plane.localEulerAngles = Vector3.up *testVal;
+        foreach (var plane in _planes)
+        {
+            plane.localEulerAngles = Vector3.Lerp(Vector3.left * 25,Vector3.left * -25,testVal);
+        }
+        //_plane.localEulerAngles = Vector3.Lerp(Vector3.left * 25,Vector3.left * -25,testVal);
     }
 }
